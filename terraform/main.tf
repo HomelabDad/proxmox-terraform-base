08dabec7c4764ee3b0737844dacb0c913eb77bbe
Jared Wilson <jwilson@spear.ai>
2025-03-07 14:56:37 -0500

    working example
    working example

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ resource "proxmox_vm_qemu" "ubuntu" {
   count       = var.vm_count
   name        = "${var.vm_name}-${count.index + 1}"
   target_node = "proxmox"         # Change to your Proxmox node name
-  clone       = "ubuntu-template" # Change to your template name
+  clone       = "test-ubuntu-template" # Change to your template name
 
   cores  = var.vm_cores
   memory = var.vm_memory
