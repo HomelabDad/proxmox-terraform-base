fc04eafb428e03e29a15bf54efa036c83324faaa
Jared Wilson <jared.d.wilson24@gmail.com>
2025-03-10 21:30:08 -0400

    Revert "Add k3s templates (#14)" (#15)
    This reverts commit 04142299b12b9b6fc172cb911fd611166449e04f.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -47,34 +47,18 @@ variable "ssh_key" {
   type        = string
 }
 
-variable "worker_ip_base" {
+variable "vm_ip_base" {
   description = "Base IP address (first 3 octets, e.g., '192.168.1.')"
   type        = string
 }
 
-variable "worker_ip_start" {
+variable "vm_ip_start" {
   description = "Starting number for the last octet of IP addresses"
   type        = number
   default     = 1
-}
-
-variable "control_node_ip" {
-  description = "IP address of the control node"
-  type        = string
 }
 
 variable "vm_gateway" {
   description = "Gateway IP address for the network"
   type        = string
 }
-
-variable "worker_count" {
-  description = "Number of K3s worker nodes to create"
-  type        = number
-  default     = 1
-}
-
-variable "private_key_path" {
-  description = "Path to the SSH private key file"
-  type        = string
-} 
